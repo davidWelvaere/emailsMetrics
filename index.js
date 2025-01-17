34b3898b662909f3d10426f0bce330d30f832f6e
david <david@welvaere.nl>
2025-01-17 17:02:03 +0100

    Why does my filter not workkk??

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const axios = require('axios')
 const { google } = require('googleapis')
 const sheets = google.sheets('v4')
 
-async function clearSheet() {
+async function clearSheet(spreadsheetId) {
     await sheets.spreadsheets.values.clear({
-        spreadsheetId: '1x5y92OVK665uccZ-QV3XoFuVMfhMHKI33G1wuUleaRw',
+        spreadsheetId,
         range: 'Emails!A2:Z'
     })
 }
 
 async function putSpreadsheetsData(hubspotData) {
-    await clearSheet()
+    const spreadsheetid = '1x5y92OVK665uccZ-QV3XoFuVMfhMHKI33G1wuUleaRw'
+    await clearSheet(spreadsheetid)
+    let values = []
+
+    for (const email of hubspotData) {
+        values.push([email.createdAt, email.name, email.stats.ratios.openratio, email.stats.ratios.clickthroughratio])
+    }
+
+    await sheets.spreadsheets.values.update({
+        spreadsheetId: spreadsheetid,
+        range: 'Emails!A2:Z',
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+            values
+        }
+    })
 }
 
 async function authenticateSpreadsheets() {
@@ -29,7 +44,7 @@ async function getHubspotData() {
     const baseurl = 'https://api.hubapi.com'
     const aggrStats = '/marketing/v3/emails'
     const now = new Date().toISOString()
-    const params = `?createdAfter=2025-01-01&includeStats=true`
+    const params = `?createdAfter=2024-01-01&includeStats=true&sort=createdAt`
     const fullurl = baseurl + aggrStats + params
     console.log(`fullurl:\n${fullurl}`)
 
@@ -40,9 +55,13 @@ async function getHubspotData() {
         }
     })
 
+    console.log(response)
+
     const filtered = response.data.results.filter(
         res => 'stats' in res && 'ratios' in res.stats && res.name.includes('NWS')
     )
+
+    console.log(filtered.length)
 
     return filtered
 }
